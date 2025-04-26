dcad59be4bb1e22c83740f222ab3b1e26b0a2c1c
Shobhit Rajpurohit <shobhit.21it465@rtu.ac.in>
2025-04-26 14:47:16 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://medical-backend-16ms.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
